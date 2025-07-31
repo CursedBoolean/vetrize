fcea44897a1f8eb8d1219ceaa2081b20408be66e
unknown <anishossameddine.belalia@univ-tiaret.dz>
2025-07-31 21:48:05 +0100

    versel speed  insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,6 +31,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} bg-[#F3F4F6] scroll-smooth antialiased`}
       >
         <Header />
+        <SpeedInsights />
         {children}
       </body>
     </html>
